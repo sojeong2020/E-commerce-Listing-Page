7baa9e7422a22dc566f5c949825bc2e1ce6e2b9b
sojeong2020 <macpark09@gmail.com>
2024-11-01 15:40:05 +0000

    add header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ProductGrid from "./components/ProductGrid.jsx";
+import Header from "./components/Header.jsx"
 
 const App = () => (
   <div className="App">
+    <Header />
     <ProductGrid />
   </div>
 );
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/styles.css';
+
+
+const Header = () => (
+    <header>
+    <h1>Product List</h1>
+    <nav>
+        <ul>
+            <li><a hre="#">Home</a></li>
+            <li><a hre="#">Bathroom</a></li>
+            <li><a hre="#">Tile</a></li>
+
+        </ul>
+    </nav>
+    <hr></hr>
+</header>
+)
+
+export default Header
